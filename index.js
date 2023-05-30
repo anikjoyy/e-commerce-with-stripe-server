767f6e822493636190753b1cf4e294134157f81c
anikjoyy <anikjoy.cse@gmail.com>
2023-05-30 20:11:02 +0600

    setup stripe cli and created webhook

--- index.js
+++ index.js
@@ -4,11 +4,23 @@ const cors = require('cors');
 require('dotenv').config({ path: './.env' });
 const createCheckoutSession = require('./api/checkout');
 
+const webhook = require('./api/webhook');
+
 const app = express();
 const port = 8080;
 
-app.use(express.json());
+app.use(
+  express.json({
+    verify: (req, res, buf) => (req['rawBody'] = buffer),
+  })
+);
+
 app.use(cors({ origin: true }));
 
 app.get('/', (req, res) => res.send('Hello World!'));
+
+app.post('/createCheckoutSession', createCheckoutSession);
+
+app.post('/webhook', webhook);
+
 app.listen(port, () => console.log('server is listening on port', port));
